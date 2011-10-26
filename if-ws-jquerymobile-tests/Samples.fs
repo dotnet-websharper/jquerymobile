f6cfd281dc5de7a84e5e72cfafcc6dd154e6bf0d
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2011-10-26 10:09:17 -0400

    Backed out changeset 58ad151080c8

--- if-ws-jquerymobile-tests/Samples.fs
+++ if-ws-jquerymobile-tests/Samples.fs
@@ -9,7 +9,7 @@
 //-----------------------------------------------------------------
 // $end{copyright}
 
-namespace IntelliFactory.WebSharper.JQuery.Mobile.Samples
+namespace IntelliFactory.WebSharper.JQuery.Mobile.Tests
 
 open IntelliFactory.WebSharper
 open IntelliFactory.WebSharper.Html
