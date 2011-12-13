ec6b763be7f28e71fd473b54144c924baa698f27
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2011-12-13 13:46:51 -0500

    Attempt to fix the jQuery dep.

--- if-ws-jquerymobile/Marker.cs
+++ if-ws-jquerymobile/Marker.cs
@@ -1,5 +1,4 @@
 using System.Web.UI;
 [assembly: IntelliFactory.WebSharper.Core.Attributes.Require(typeof(IntelliFactory.WebSharper.JQuery.Mobile.Resources.JQueryMobile))]
-[assembly: IntelliFactory.WebSharper.Core.Attributes.Require(typeof(IntelliFactory.WebSharper.JQuery.Resources.JQuery))]
 
 
--- if-ws-jquerymobile/Resources.cs
+++ if-ws-jquerymobile/Resources.cs
@@ -3,6 +3,7 @@ using IntelliFactory.WebSharper;
 
 namespace IntelliFactory.WebSharper.JQuery.Mobile.Resources
 {
+    [IntelliFactory.WebSharper.Core.Attributes.Require(typeof(IntelliFactory.WebSharper.JQuery.Resources.JQuery))]
     public class JQueryMobile : IntelliFactory.WebSharper.Core.Resources.BaseResource
     {
         public JQueryMobile() : base("http://code.jquery.com/mobile/1.0b2", "/jquery.mobile-1.0b2.min.js", "/jquery.mobile-1.0b2.min.css") { }
