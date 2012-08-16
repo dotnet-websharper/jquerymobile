701a8faaba9c5139513bd39e42b2f84ae36c270b
Loic Denuziere <loic.denuziere@intellifactory.com>
2012-08-16 17:31:09 +0100

    Use stable version of JQueryMobile.

--- if-ws-jquerymobile/Resources.cs
+++ if-ws-jquerymobile/Resources.cs
@@ -6,8 +6,8 @@ namespace IntelliFactory.WebSharper.JQuery.Mobile.Resources
     [IntelliFactory.WebSharper.Core.Attributes.Require(typeof(IntelliFactory.WebSharper.JQuery.Resources.JQuery))]
     public class JQueryMobile : IntelliFactory.WebSharper.Core.Resources.BaseResource
     {
-        public JQueryMobile() : base("http://code.jquery.com/mobile/1.2.0-alpha.1",
-            "/jquery.mobile-1.2.0-alpha.1.min.js",
-            "/jquery.mobile-1.2.0-alpha.1.min.css") {}
+        public JQueryMobile() : base("http://code.jquery.com/mobile/1.1.1",
+            "/jquery.mobile-1.1.1.min.js",
+            "/jquery.mobile-1.1.1.min.css") {}
     }
 }
