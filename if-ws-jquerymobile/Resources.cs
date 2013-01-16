8b52ded42afe8c11d43579d0e4dfadf76412e18c
Csaba Hruska <csaba.hruska@gmail.com>
2013-01-16 12:50:55 +0100

    update jquery-mobile resource reference to 1.2 version

--- if-ws-jquerymobile/Resources.cs
+++ if-ws-jquerymobile/Resources.cs
@@ -6,8 +6,9 @@ namespace IntelliFactory.WebSharper.JQuery.Mobile.Resources
     [IntelliFactory.WebSharper.Core.Attributes.Require(typeof(IntelliFactory.WebSharper.JQuery.Resources.JQuery))]
     public class JQueryMobile : IntelliFactory.WebSharper.Core.Resources.BaseResource
     {
-        public JQueryMobile() : base("http://code.jquery.com/mobile/1.1.1",
-            "/jquery.mobile-1.1.1.min.js",
-            "/jquery.mobile-1.1.1.min.css") {}
+        public JQueryMobile()
+            : base("http://code.jquery.com/mobile/1.2.0",
+            "/jquery.mobile-1.2.0.min.js",
+            "/jquery.mobile-1.2.0.min.css") { }
     }
 }
