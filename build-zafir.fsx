13c019eb827a41d611009b876ff324cfa8d5dd48
István Gansperger <gansperger@gmail.com>
2015-12-15 14:07:09 +0100

    Add zafir build

---
+++ build-zafir.fsx
@@ -0,0 +1,38 @@
+#load "tools/includes.fsx"
+open IntelliFactory.Build
+
+let bt =
+    BuildTool().PackageId("Zafir.JQueryMobile")
+        .VersionFrom("Zafir")
+        .WithFSharpVersion(FSharpVersion.FSharp30)
+        .WithFramework(fun fw -> fw.Net40)
+
+let ext =
+    bt.Zafir.Extension("WebSharper.JQuery.Mobile")
+    |> fun main -> main.SourcesFromProject()
+
+let tests =
+    bt.Zafir.HtmlWebsite("WebSharper.JQuery.Mobile.Tests")
+    |> fun tests ->
+        tests.SourcesFromProject().References(fun r ->
+            [
+                r.NuGet("Zafir.Html").Reference()
+                r.Project ext
+            ])
+
+bt.Solution [
+    ext
+    tests
+
+    bt.NuGet.CreatePackage()
+        .Configure(fun c ->
+            { c with
+                Title = Some "Zafir.JQueryMobile-1.4.2"
+                LicenseUrl = Some "http://websharper.com/licensing"
+                ProjectUrl = Some "https://github.com/intellifactory/websharper.jquerymobile"
+                Description = "Zafir Extensions for JQuery Mobile 1.4.2"
+                RequiresLicenseAcceptance = true })
+        .Add(ext)
+
+]
+|> bt.Dispatch
